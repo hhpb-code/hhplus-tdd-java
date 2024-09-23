06493c07bdf7f3268da6490f25b8a67a466673af
kilhyeonjun <kboxstar@gmail.com>
2024-09-23 23:51:27 +0900

    feat: BusinessException class 생성
    - BusinessException class 생성 (RuntimeException)
    - ErrorCode interface 생성

---
+++ src/main/java/io/hhplus/tdd/error/BusinessException.java
@@ -0,0 +1,16 @@
+package io.hhplus.tdd.error;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+  private final ErrorCode errorCode;
+  private final Object[] args;
+
+  public BusinessException(ErrorCode errorCode, Object... args) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+    this.args = args;
+  }
+}
