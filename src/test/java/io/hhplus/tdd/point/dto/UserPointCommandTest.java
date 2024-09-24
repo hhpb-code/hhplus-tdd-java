d5cbb463863a6858677200b1174ffddfeed4d4de
kilhyeonjun <kboxstar@gmail.com>
2024-09-24 17:37:41 +0900

    test: UserPointCommand 테스트 코드 추가
    - Charge 생성 test code
    - Use 생성 및 validation test code
    - GetUserPoint 생성 및 validation test code

--- src/test/java/io/hhplus/tdd/point/dto/UserPointCommandTest.java
+++ src/test/java/io/hhplus/tdd/point/dto/UserPointCommandTest.java
@@ -55,4 +55,106 @@ class UserPointCommandTest {
     assertThat(result.getMessage()).isEqualTo(PointErrorCode.INVALID_AMOUNT.getMessage());
   }
 
+  @Test
+  @DisplayName("UserPointCommand.Charge 생성 성공")
+  void shouldCreateUserPointCommandCharge() {
+    // given
+    final Long userId = 1L;
+    final Long amount = 100L;
+
+    // when
+    final UserPointCommand.Charge result = UserPointCommand.Charge.from(userId, amount);
+
+    // then
+    assertThat(result.userId()).isEqualTo(userId);
+    assertThat(result.amount()).isEqualTo(amount);
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.Use 생성 실패 - userId가 null")
+  void shouldFailToCreateUserPointCommandUseWhenUserIdIsNull() {
+    // given
+    final Long userId = null;
+    final Long amount = 100L;
+
+    // when
+    final BusinessException result = assertThrows(BusinessException.class,
+        () -> UserPointCommand.Use.from(userId, amount));
+
+    // then
+    assertThat(result.getMessage()).isEqualTo(PointErrorCode.INVALID_USER_ID.getMessage());
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.Use 생성 실패 - amount 가 null")
+  void shouldFailToCreateUserPointCommandUseWhenAmountIsNull() {
+    // given
+    final Long userId = 1L;
+    final Long amount = null;
+
+    // when
+    final BusinessException result = assertThrows(BusinessException.class,
+        () -> UserPointCommand.Use.from(userId, amount));
+
+    // then
+    assertThat(result.getMessage()).isEqualTo(PointErrorCode.INVALID_AMOUNT.getMessage());
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.Use 생성 실패 - amount 가 0이하")
+  void shouldFailToCreateUserPointCommandUseWhenAmountIsLessThanZero() {
+    // given
+    final Long userId = 1L;
+    final Long amount = 0L;
+
+    // when
+    final BusinessException result = assertThrows(BusinessException.class,
+        () -> UserPointCommand.Use.from(userId, amount));
+
+    // then
+    assertThat(result.getMessage()).isEqualTo(PointErrorCode.INVALID_AMOUNT.getMessage());
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.Use 생성 성공")
+  void shouldCreateUserPointCommandUse() {
+    // given
+    final Long userId = 1L;
+    final Long amount = 100L;
+
+    // when
+    final UserPointCommand.Use result = UserPointCommand.Use.from(userId, amount);
+
+    // then
+    assertThat(result.userId()).isEqualTo(userId);
+    assertThat(result.amount()).isEqualTo(amount);
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.GetUserPoint 생성 실패 - userId가 null")
+  void shouldFailToCreateUserPointCommandGetUserPointWhenUserIdIsNull() {
+    // given
+    final Long userId = null;
+
+    // when
+    final BusinessException result = assertThrows(BusinessException.class,
+        () -> UserPointCommand.GetUserPoint.from(userId));
+
+    // then
+    assertThat(result.getMessage()).isEqualTo(PointErrorCode.INVALID_USER_ID.getMessage());
+  }
+
+  @Test
+  @DisplayName("UserPointCommand.GetUserPoint 생성 성공")
+  void shouldCreateUserPointCommandGetUserPoint() {
+    // given
+    final Long userId = 1L;
+
+    // when
+    final UserPointCommand.GetUserPoint result = UserPointCommand.GetUserPoint.from(userId);
+
+    // then
+    assertThat(result.userId()).isEqualTo(userId);
+  }
+
 }
